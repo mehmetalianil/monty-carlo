df2445f169bda0a802ef799732ba8213e88a7195
Mehmet Ali Anil <mehmet.ali.anil@gmail.com>
2011-08-13 22:06:40 +0300

    Machine code started

---
+++ src/machine/README.md
@@ -0,0 +1,6 @@
+Machine
+=======
+
+The machine package is the core module in which the Monte Carlo simulation will 
+run. It will be linked to the random number generator, the state machines, the 
+state definitions, such that it will run tailored to the purpose.
--- src/monte-carlo.py
+++ src/monte-carlo.py
@@ -9,7 +9,6 @@ The Monte-Carlo Simulation Engine
 '''
 
 import numpy as num
-import profile
 import pylab
 from mpl_toolkits.mplot3d import Axes3D
 import math
@@ -196,35 +195,7 @@ class ising_2d(object):
         for neighbour in state_neigh[ctr_y][ctr_x]:
             energy = energy + state[neighbour[0]][neighbour[1]]
         delta_E = 2*spin*energy
-    
-def monte_carlo_2d(system):
-    if type(system) != system:
-        print "The Monte Carlo engine must have the input system of type system."
-        print "Please prepare your state as an system type object."
-    else:
-        
-        print "Initializing the algorithm..."
-        
-        magnetization_beta = []
-        
-        for dependency in system.dependencies:
-            for dep_name in dependency:
-                magnetization_list = num.array([])
-                energy_list = num.array([])
-                standard_dev = 0
-                err=0
-                ctr=0
-                print "Simulating the system with beta = "+str(beta)
-                while  (err > 20 or err == 0) or ctr<20:
-                    (state,params) = monte_carlo_iter(neigh,state,beta,plot=False)
-                    magnetization_list = num.append(magnetization_list,params[0])
-                    energy_list = num.append(energy_list,params[1])
-                    # We take the standard deviation of the last four  divided by the 
-                    standard_dev = num.std(energy_list[-4:])
-                    err = standard_dev/num.mean(energy_list[-4:])
-                    print "    ["+str(ctr)+"]Standard Deviation: " + str(standard_dev)  
-                    ctr = ctr + 1      
-                magnetization_beta.append(magnetization_list[-1])
+
         
 class system(object):
     """   
