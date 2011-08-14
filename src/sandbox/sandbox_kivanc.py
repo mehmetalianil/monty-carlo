18728eb5dd50b244229d28621db47cd3fba4e39e
Mehmet Ali Anil <mehmet.ali.anil@gmail.com>
2011-08-14 13:36:52 +0300

    sandbox for kivanc is created

---
+++ src/sandbox/sandbox_kivanc.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+__author__ = "Kivanc Esat"
+__copyright__ = ""
+__credits__ = ["Kivanc Esat","Mehmet Ali Anil"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Kivanc Esat"
+__email__ = ""
+__status__ = "Production"
+
+import numpy as num
+import pylab
+from mpl_toolkits.mplot3d import Axes3D
+import math
+import matplotlib.pyplot as plt     # Plotting
+import profile                      # For performance analysis
+
+
+if __name__ == '__main__':
+    print 'Hello Word'
